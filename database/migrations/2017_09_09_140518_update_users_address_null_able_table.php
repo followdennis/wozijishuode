0a3100f08bf7a20e12353d7d7c34d71959bd52c5
followdennis <963132814@qq.com>
2017-09-09 22:13:06 +0800

    u

--- database/migrations/2017_09_09_140518_update_users_address_null_able_table.php
+++ database/migrations/2017_09_09_140518_update_users_address_null_able_table.php
@@ -28,6 +28,7 @@ class UpdateUsersAddressNullAbleTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
+            $table->string('address')->default('')->change();
         });
     }
 }
