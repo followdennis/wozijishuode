eb3812f4f7aef620b7f9b88e16d26c10c12f0fff
followdennis <963132814@qq.com>
2017-09-20 23:59:24 +0800

    aaa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,6 @@
 </head>
 <div clss="row">
 <input type="input" value="hello" class="form-group">
-    <a href="#" class="btn btn-default">hello</a>
+    <a href="#" class="btn btn-default">aaaa</a>
 </div>
 </html>
