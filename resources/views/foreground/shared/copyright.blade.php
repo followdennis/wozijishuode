4eb04e44852878c11a08d556ee7c1f230f211aeb
followdennis <963132814@qq.com>
2019-12-12 19:25:33 +0800

    版权

--- resources/views/foreground/shared/copyright.blade.php
+++ resources/views/foreground/shared/copyright.blade.php
@@ -6,7 +6,7 @@
     </div>
     <div class="panel-body label_style">
         <div>
-            ©2018 wozijishuode.com我自己说的版权所有
+            ©2018 wozijishuode.com我自己说的版权所有 <br/><a href="http://www.miitbeian.gov.cn/">苏ICP备17001219号-1</a>
         </div>
     </div>
 </div>
